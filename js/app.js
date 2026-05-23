2a41386f6db0b51e16bf4e467d7652af8811bc8a
elpromoonline-prog <elpromoonline@gmail.com>
2026-05-22 21:24:39 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,10 +3,7 @@ async function chamarAPI(messages) {
     const res = await fetch(API_CONFIG.endpoint, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
-        'x-api-key': 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,
-        'anthropic-version': '2023-06-01',
-        'anthropic-dangerous-direct-browser-access': 'true'
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         model: API_CONFIG.model,
